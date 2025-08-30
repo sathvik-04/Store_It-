6145e482952ebfc288814614f9da4afe147caf19
sathvik-04 <sathvikshetty0404@gmail.com>
2025-08-31 02:24:46 +0530

    added components

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <div className="flex-center h-screen ">
-      <h1 className="h1">Hello world!</h1> 
-    </div>
-  );   
-}   
